6daa1b7e4abf5e4d694b17aadbd03d7dfff01cbf
Arnab Jana <arnab.jana.720@gmail.com>
2025-07-31 06:08:03 +0530

    Responsiveness issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import LoadingScreen from "./components/LoadingScreen";
 import "./index.css";
 import Navbar from "./components/Navbar";
--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -72,7 +72,7 @@ const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 max-w-screen">
           <h2 className="text-3xl font-bold mb-8 text-center bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">
             Get in Touch
           </h2>
